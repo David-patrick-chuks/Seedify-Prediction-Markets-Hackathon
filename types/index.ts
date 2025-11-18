21b4987ebbdaeb30c8928935075e312eee28815b
David Patrick Chuks <161928481+David-patrick-chuks@users.noreply.github.com>
2025-11-18 13:52:44 +0100

    Add Hardhat network config for BNB Mainnet

--- types/index.ts
+++ types/index.ts
@@ -1,16 +1,16 @@
-export type Round = {
-    epoch: bigint;
-    startTimestamp: bigint;
-    lockTimestamp: bigint;
-    closeTimestamp: bigint;
-    lockPrice: bigint;
-    closePrice: bigint;
-    lockOracleId: bigint;
-    closeOracleId: bigint;
-    totalAmount: bigint;
-    bullAmount: bigint;
-    bearAmount: bigint;
-    rewardBaseCalAmount: bigint;
-    rewardAmount: bigint;
-    oracleCalled: boolean;
+export type Round = {
+    epoch: bigint;
+    startTimestamp: bigint;
+    lockTimestamp: bigint;
+    closeTimestamp: bigint;
+    lockPrice: bigint;
+    closePrice: bigint;
+    lockOracleId: bigint;
+    closeOracleId: bigint;
+    totalAmount: bigint;
+    bullAmount: bigint;
+    bearAmount: bigint;
+    rewardBaseCalAmount: bigint;
+    rewardAmount: bigint;
+    oracleCalled: boolean;
 };
